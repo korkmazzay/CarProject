ccc41912c6f8d36be0931870a89232a9ead37dc3
korkmaz <korkmazzay@gmail.com>
2021-06-12 23:27:50 -0700

    SECOND

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,10 +3,6 @@ package com.company;
 public class Main {
     public static void main(String[] args) {
     car audi=new car();
-    audi.color="Red";
-    audi.number="111";
-    audi.model="A3";
-    audi.door="2";
 
 
     }
